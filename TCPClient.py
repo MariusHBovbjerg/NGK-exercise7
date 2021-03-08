bcd66c588846eb0b40c4e493904c24b25e6c7185
Smell-ya-later <marius@hambro.eu>
2021-03-08 15:28:18 +0100

    final commit

--- TCPClient.py
+++ TCPClient.py
@@ -3,15 +3,19 @@ import socket
 from lib import Lib
 
 
-serverAddrPort = ("10.0.0.1",9000)
-BUFSIZE = 1000
+serverAddrPort = ("10.0.0.1",9001)
+BUFSIZE = 1024
 
 def main(argv):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         print("Write 'u' for uptime or 'l' for load average")
         msg = input()
         print("Sending message")
-        s.sendto(msg.encode(),serverAddrPort)
+        bytesToSend = str.encode(msg)
+        s.sendto(bytesToSend,serverAddrPort)
+        data, addr = s.recvfrom(BUFSIZE)
+        print("connection from: {}".format(addr))
+        print(data.decode())
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- TCPServer.py
+++ TCPServer.py
@@ -3,7 +3,7 @@ import socket
 from lib import Lib
 
 HOST = ""
-PORT = 9000
+PORT = 9001
 BUFSIZE = 1024
 
 def checkClientInput(x):
@@ -19,37 +19,35 @@ def main(argv):
         s.bind((HOST, PORT))
         print("Server is listening.")
         while True:
-                
-            data = s.recvfrom(BUFSIZE)
-            datamsg = data[0]
-            dataaddr = data[1]
+            print("yo")
+            data, addr = s.recvfrom(BUFSIZE)
 
-            print("connection from: {}".format(dataaddr))
-
-            request = checkClientInput(datamsg)
+            print("connection from: {}".format(addr))
+            request = checkClientInput(data.decode())
 
             if request == 1:
 
                 print("l or L was received")
+                sendFile("/proc/loadavg",s,addr)
                 
             elif request == 2:
                 
                 print("u or U was received")
+                sendFile("/proc/uptime",s,addr)
 
             else:
                 print("Neither was received")
 
 
-def sendFile(fileName, conn):
+def sendFile(fileName, conn,addr):
     file = open(fileName,"r")
     TEXTBUF = file.read(BUFSIZE)
     print("Read:",len(TEXTBUF), ".")
     while(TEXTBUF):
         #Lib.writeTextTCP(TEXTBUF,conn)
-        conn.send(TEXTBUF.encode())
+        conn.sendto(TEXTBUF.encode(),addr)
         TEXTBUF = file.read(BUFSIZE)
         print("Read:",len(TEXTBUF),".")
-    conn.send(b'\0')
     file.close
     print("File closed.")
 
